f9f0cbf94ddabbbd8f70d559b510bcd942cc2ce5
yajiji <yajiji5@gmail.com>
2014-06-16 22:44:34 +0900

    名称変更

--- html/js/main.js
+++ html/js/main.js
@@ -1,7 +1,7 @@
   $(function() {
     $("#socialbuttons .twitter").socialbutton("twitter", {
       button : "horizontal",
-      text   : "4年に1度だし、みんなで応援するのも良いかも！ー“日本代表を応援したい”全ての人へ『QUESTION』",
+      text   : "逆境に立ち向かう日本代表をみんなで応援するのも良いかも！ー“日本代表を応援したい”全ての人へ『QUESTION』",
       url    : "http://cheer-up-japan.com",
     }).width(105);
 
